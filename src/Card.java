71daca4efbe86e4d6cf0e5a073bbb9a2ad012493
B. Alfanous <b.alfanous@outlook.com>
2021-11-29 22:44:17 -0500

    Added Card Class

---
+++ src/Card.java
@@ -0,0 +1,36 @@
+
+/**
+ * @author B. Alfanous
+ * CENG 320 Lab 04
+ */ 
+
+import java.io.Serializable;
+
+public class Card implements Serializable{
+    private String suit;
+    private String rank;
+    private int value = 0;
+
+    public Card(){}    
+    public Card(String rank, String suit){
+        this.suit = suit;
+        this.rank = rank;
+    }
+    
+    
+    public void setValue(int value){
+     this.value = value;
+    }
+     
+    public int getValue(){
+        return this.value;
+    }
+        
+    @Override
+    public String toString(){
+        return rank + " Of " + suit;
+    }
+    
+    
+    
+}
